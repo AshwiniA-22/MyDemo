f38d38077b79f4dd23e8fedac29d73421bec3e43
AshwiniA-22 <ashwiniavhad229@gmail.com>
2024-05-24 15:27:51 +0530

    join 2 strings

---
+++ src/com/TS/MyPrograms/ConcatString.java
@@ -0,0 +1,12 @@
+package com.TS.MyPrograms;
+
+public class ConcatString {
+
+	
+	public static void main(String[] args) {
+		String s1="I love";
+		String s2=" India";
+		
+		System.out.println(s1.concat(s2));
+	}
+}
